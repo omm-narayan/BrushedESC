2e7bd6bad7079df8493619c44205a8d3aa51b09f
omm-narayan <omm.codes@gmail.com>
2025-09-07 12:51:15 +0530

    update

--- atmega.ino
+++ atmega.ino
@@ -2,59 +2,63 @@
 
 #define MIDDLE_POINT 1500
 
-// -------- Channel A --------
 #define OUT1A 3
 #define OUT2A 5
 #define INA   2
 
-// -------- Channel B --------
 #define OUT1B 6
 #define OUT2B 9
 #define INB   4
 
-// Channel A variables
-volatile int pre_pwm_valueA = 0;
-volatile int pwm_valueA = 0;
-volatile int prev_timeA = 0;
+volatile unsigned long prev_timeA = 0;
+volatile int pwm_valueA = 1500;
 
-// Channel B variables
-volatile int pre_pwm_valueB = 0;
-volatile int pwm_valueB = 0;
-volatile int prev_timeB = 0;
+volatile unsigned long prev_timeB = 0;
+volatile int pwm_valueB = 1500;
 
 void setup() {
-  // Channel A setup
+  Serial.begin(9600);
+
   pinMode(OUT1A, OUTPUT);
   pinMode(OUT2A, OUTPUT);
-  attachPCINT(INA, risingA, RISING);
+  pinMode(INA, INPUT);
 
-  // Channel B setup
   pinMode(OUT1B, OUTPUT);
   pinMode(OUT2B, OUTPUT);
-  attachPCINT(INB, risingB, RISING);
+  pinMode(INB, INPUT);
+
+  attachPCINT(digitalPinToPCINT(INA), isrA, CHANGE);
+  attachPCINT(digitalPinToPCINT(INB), isrB, CHANGE);
 }
 
-void loop() {}
-
-// ----------------- Channel A -----------------
-void risingA() {
-  attachPCINT(INA, fallingA, FALLING);
-  prev_timeA = micros();
+void loop() {
+  // Debug monitor
+  Serial.print("CH A: "); Serial.print(pwm_valueA);
+  Serial.print("   CH B: "); Serial.println(pwm_valueB);
+  delay(200);
 }
 
-void fallingA() {
-  attachPCINT(INA, risingA, RISING);
-  pre_pwm_valueA = pwm_valueA;
-  pwm_valueA = micros() - prev_timeA;
-  if (pwm_valueA < 1000 || pwm_valueA > 1900) {
-    pwm_valueA = pre_pwm_valueA;
+void isrA() {
+  if (digitalRead(INA)) {
+    prev_timeA = micros();
+  } else {
+    pwm_valueA = micros() - prev_timeA;
+    PWM_outA(pwm_valueA);
   }
-  PWM_outA(pwm_valueA);
+}
+
+void isrB() {
+  if (digitalRead(INB)) {
+    prev_timeB = micros();
+  } else {
+    pwm_valueB = micros() - prev_timeB;
+    PWM_outB(pwm_valueB);
+  }
 }
 
 void PWM_outA(int speeds) {
   int tempSpeeds = map(abs(speeds - MIDDLE_POINT), 0, 500, 0, 255);
-  if (tempSpeeds > 255) tempSpeeds = 255;
+  tempSpeeds = constrain(tempSpeeds, 0, 255);
 
   if (speeds > MIDDLE_POINT) {
     analogWrite(OUT1A, tempSpeeds);
@@ -65,25 +69,9 @@ void PWM_outA(int speeds) {
   }
 }
 
-// ----------------- Channel B -----------------
-void risingB() {
-  attachPCINT(INB, fallingB, FALLING);
-  prev_timeB = micros();
-}
-
-void fallingB() {
-  attachPCINT(INB, risingB, RISING);
-  pre_pwm_valueB = pwm_valueB;
-  pwm_valueB = micros() - prev_timeB;
-  if (pwm_valueB < 1000 || pwm_valueB > 1900) {
-    pwm_valueB = pre_pwm_valueB;
-  }
-  PWM_outB(pwm_valueB);
-}
-
 void PWM_outB(int speeds) {
   int tempSpeeds = map(abs(speeds - MIDDLE_POINT), 0, 500, 0, 255);
-  if (tempSpeeds > 255) tempSpeeds = 255;
+  tempSpeeds = constrain(tempSpeeds, 0, 255);
 
   if (speeds > MIDDLE_POINT) {
     analogWrite(OUT1B, tempSpeeds);
